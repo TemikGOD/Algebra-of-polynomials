945c65a0bb1f3a8028899c4a33e2eb392661c188
Денисов Артём <90596415+TemikGOD@users.noreply.github.com>
2023-04-25 21:12:45 +0300

    Добавил метод isInTable

--- TableInterface.h
+++ TableInterface.h
@@ -6,6 +6,7 @@ protected:
 	virtual void addPolynomial(const Polynomial& _pol) = 0;
 	virtual void deletePolynomial(const Polynomial& _pol) = 0;
 	virtual Polynomial search(const unsigned int& _key) = 0;
+	virtual bool isInTable(const Polynomial& _pol) = 0;
 };
 
 
