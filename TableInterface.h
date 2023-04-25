cb8395517c25c49562a9907556812bccee47cc94
Денисов Артём <90596415+TemikGOD@users.noreply.github.com>
2023-04-25 20:55:24 +0300

    Add files via upload

---
+++ TableInterface.h
@@ -0,0 +1,11 @@
+#pragma once
+class TableInterface
+{
+protected:
+	unsigned int key;
+	virtual void addPolynomial(const Polynomial& _pol) = 0;
+	virtual void deletePolynomial(const Polynomial& _pol) = 0;
+	virtual Polynomial search(const unsigned int& _key) = 0;
+};
+
+
