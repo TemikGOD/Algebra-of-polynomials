57c0d360f04c4c96daf6fe1beea1b2abbf2f3ed3
Денисов Артём <90596415+TemikGOD@users.noreply.github.com>
2023-04-28 20:34:31 +0300

    Исправил

--- TableInterface.h
+++ TableInterface.h
@@ -2,10 +2,10 @@
 class TableInterface
 {
 protected:
+	unsigned int key;
 	virtual void addPolynomial(const Polynomial& _pol) = 0;
 	virtual void deletePolynomial(const Polynomial& _pol) = 0;
 	virtual Polynomial search(const unsigned int& _key) = 0;
-	virtual bool isInTable(const Polynomial& _pol) = 0;
 };
 
 
