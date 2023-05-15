4653ac9ac94e2237e0274d44f115a4c2a646a538
savchukPR2 <90461166+savchukPR2@users.noreply.github.com>
2023-05-15 20:29:59 +0300

    Update OrderedArray.hpp
    Сделал бинарный поиск

--- Polymath/Tables/OrderedArray.hpp
+++ Polymath/Tables/OrderedArray.hpp
@@ -1,91 +1,83 @@
 #include <vector>
-#include "TableInterface.hpp"
-#include "MyExcepions.hpp"
 using namespace std;
+
+#include "TableInterface.hpp"
+#include "MyExeptions.hpp"
 
 class OrderedTable : public TableInterface{
 private:
     vector<pair<string, Poly*>> OrdArray;
 
-    void SortTable(vector<pair<string, Poly*>>& vec, int left, int right) {
-        if (left >= right) {
-            return;
-        }
-
-        int mid = left + (right - left) / 2;
-        SortTable(vec, left, mid);
-        SortTable(vec, mid + 1, right);
-
-        vector<pair<string, Poly*>> left_vec(vec.begin() + left, vec.begin() + mid + 1);
-        vector<pair<string, Poly*>> right_vec(vec.begin() + mid + 1, vec.begin() + right + 1);
-
-        int i = 0, j = 0;
-        for (int k = left; k <= right; k++) {
-            if (j >= right_vec.size() || (i < left_vec.size() && left_vec[i].first <= right_vec[j].first)) {
-                vec[k] = left_vec[i++];
-            }
-            else {
-                vec[k] = right_vec[j++];
-            }
-        }
-
-
-    }
 public:
     OrderedTable() {}
-
-    virtual ~OrderedTable()
-    {
-        for(const auto &del: OrdArray)
-            delete del.second;
-        OrdArray.clear();
-    }
-
-    Poly getPol(int index) override { return *OrdArray[index].second; }
-    string getKey(int index) override { return OrdArray[index].first; }
+       
     
     void addPolynomial(const std::string& _key, Poly* const _polynomial) override {
-        bool addedCheck = false;
-        try
-        {
-            searchPolynomial(_key);
+        // ищем позицию, на которую нужно вставить новый элемент, с помощью бинарного поиска
+        int left = 0, right = OrdArray.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (OrdArray[mid].first == _key) {
+                throw AddException("A polynomial with this name is already in the table");
+            }
+            else if (OrdArray[mid].first < _key) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
         }
-        catch (NothingFoundException& exp)
-        {
-            OrdArray.push_back({ _key, _polynomial });
-            SortTable(OrdArray, 0, OrdArray.size() - 1);
-            addedCheck = true;
-        }
-        if (!addedCheck)
-        {
-            throw AddException("A polynomial with this name is already in the table");
-        }
+        // вставляем новый элемент на найденную позицию
+        OrdArray.insert(OrdArray.begin() + left, { _key, _polynomial });
     }
 
     void deletePolynomial(const std::string& _key) override {
         int temp = -1;
+        vector<pair<string, Poly*>> tempArray;
         if (OrdArray.size() == 0)
             throw EmptyTableException("Empty table");
 
-        for (int i = 0; i < OrdArray.size(); i++) {
-            if (OrdArray[i].first == _key) {
-                OrdArray.erase(OrdArray.begin() + i);
-                temp = 1;
+        int left = 0, right = OrdArray.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (OrdArray[mid].first == _key) {
+                temp = mid;
+                break;
+            }
+            else if (OrdArray[mid].first < _key) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
             }
         }
+        for (int i = 0; i < temp; i++) {
+            tempArray.push_back(OrdArray[i]);
+        }
+        for (int i = temp+1; i < OrdArray.size(); i++) {
+            tempArray.push_back(OrdArray[i]);
+        }
+        OrdArray = move(tempArray);
         if (temp == -1)
             throw NothingFoundException("No polynomial found with this key");
     }
     
-    Poly searchPolynomial(const std::string& _key) override {
+    Poly searchPolynomial(const std::string& _key) {
         int temp = -1;
-        for (int i = 0; i < OrdArray.size(); i++) {
-            if (OrdArray[i].first == _key) {
-                return *OrdArray[i].second;
-                temp = 1;
+        int left = 0, right = OrdArray.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (OrdArray[mid].first == _key) {
+                return *OrdArray[mid].second;
+            }
+            else if (OrdArray[mid].first < _key) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
             }
         }
         if (temp == -1)
-            throw NothingFoundException("There is no such key in the table");
+            throw NoPolyException("Полином с таким ключом не найден");
     }
 };
